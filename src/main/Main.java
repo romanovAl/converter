b3db8e3d17e34ce2d72e6a5c302d7c756c36d3d9
romanovAl <43294059+romanovAl@users.noreply.github.com>
2018-09-21 22:08:08 +0500

    Some commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,15 +4,39 @@ import java.util.Scanner;
 
 public class Main {
 
-    Scanner scan = new Scanner(System.in);
-
-    long decVar = scan.nextLong();
-
     public static void main(String[] args) {
-        if(varTypeChecker)
+
+        System.out.println("Введите ваше число");
+
+        Scanner scan = new Scanner(System.in);
+
+        long decVar = scan.nextLong();
+
+        if(varTypeChecker(-128,127,decVar)){
+            System.out.println("byte is ok");
+        }
+
+        if(varTypeChecker(-32768,32767,decVar)){
+            System.out.println("short is ok");
+        }
+
+        if(varTypeChecker(-2147483648,2147483647,decVar)){
+            System.out.println("int is ok");
+        }
+
+        if(varTypeChecker(-9223372036854775808L,9223372036854775807L,decVar)){
+            System.out.println("long is ok");
+        }
+
+        while(Math.IEEEremainder(decVar, 2) < 1){
+
+            Math.IEEEremainder(decVar,2);
+
+
+        }
     }
 
-    boolean varTypeChecker(long a, long b, long number){
+    private static boolean varTypeChecker(long a, long b, long number){
         if(number >= a & number <= b ){
             return true;
         }
