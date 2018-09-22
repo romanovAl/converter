7bc42db685952015bec15907168b26c5f5a0fb6f
romanovAl <43294059+romanovAl@users.noreply.github.com>
2018-09-22 16:11:09 +0500

    version 1.0

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -28,18 +29,52 @@ public class Main {
             System.out.println("long is ok");
         }
 
-        while(Math.IEEEremainder(decVar, 2) < 1){
+        reverseMass(decToBin(decVar));
 
-            Math.IEEEremainder(decVar,2);
+    }
+    private static int[] decToBin(long number){ //приводит к двоичной системе
+        int[] massBinNum = new int[numberOfDiv(number)];
+        long number2 = number;
+        for(int i = 0; i < numberOfDiv(number2);i++){
 
+            if((number / (double)2) == (int)Math.floor(number/2)){
+                massBinNum[i] = 0;
+                number = (long)Math.floor(number/2);
+            } else{
+                massBinNum[i] = 1;
+                number = (long)Math.floor(number/2);
+            }
 
         }
+        return massBinNum;
+
     }
 
-    private static boolean varTypeChecker(long a, long b, long number){
-        if(number >= a & number <= b ){
-            return true;
+
+    private static int[] reverseMass(int[]massive){ //Переворачивает массив
+        int[] finalMassive = new int[massive.length];
+        for(int i = 0; i < massive.length; i++ ){
+            finalMassive[i] = massive[massive.length - 1 - i];
+//            System.out.println(Arrays.toString(finalMassive));
         }
-        return false;
+        for(int i = 0; i< finalMassive.length; i++){
+            System.out.print(finalMassive[i]);
+        }
+        return finalMassive;
     }
+
+
+    private static int numberOfDiv(long number){  //Эта функция считает количество знаков в двоичном числе(длинна массива)
+        int i = 1;
+        while(Math.floor(number / 2) >= 1){
+            number = (long)Math.floor(number / 2);
+            i++;
+        }
+        return i;
+    }
+
+    private static boolean varTypeChecker(long a, long b, long number){  //Проверяет в каком диапазоне число
+        return number >= a & number <= b;
+    }
+
 }
